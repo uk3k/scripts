c1a7abe9e5aa7d4d53b6cc1347550754bbc4a86d
Paul Weidland <github@uk3k.de>
2026-02-20 08:59:05 +0100

    Update install-kvm-qemu.sh

--- install-kvm-qemu.sh
+++ install-kvm-qemu.sh
@@ -8,9 +8,10 @@
 #https://medium.com/@santoshgarole/migrating-virtual-machines-a-guide-to-migrating-vmware-vms-to-kvm-dd6860c52adc
 
 #Hint: Use nm-connection-editor for creating the bridge  instead of the KDE provided applet, this seems to be unreliable
+#Convert: qemu-img convert -O qcow2 VVmX-000001.vmdk KVmX.qcow2
 
 sudo apt update
-sudo apt install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon virt-manager nm-connection-editor resolvconf-y
+sudo apt install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon virt-manager nm-connection-editor resolvconf qemu-utils -y
 sudo virsh net-start default
 sudo virsh net-autostart default
 sudo modprobe vhost_net
@@ -30,4 +31,6 @@ read -p "disable connection-NAME (of $device) before enbaling the bridge: " name
 sudo nmcli con down "$name"
 sudo nmcli con up br0
 read -p "please reboot to finish setup [Enter --> got it]: "
+
+
 
