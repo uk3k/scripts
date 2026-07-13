de4228b23ecc87c40215aa16e3e14a8a4dd3b507
Paul Weidland <github@uk3k.de>
2026-07-13 08:23:04 +0200

    Update MOK-nvidia-update.sh

--- MOK-nvidia-update.sh
+++ MOK-nvidia-update.sh
@@ -4,36 +4,34 @@ echo "MOK-signed Nvidia-Drivers Update Script by uk3k.de"
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
-else
+fi
   localfile=$(ls | grep NVIDIA-Linux | tail -1)
   echo "found local package $localfile", use this?"
   read -p ' (y)es or (d)ownlod from nvidia? [y] ' mode
   if [ $mode == "y" ] || [ -z $mode ]
     then echo "using local file $localfile"
-  fi
   else
     echo "trying to download current driver automatically..."
     read -p 'whats the current nvidia driver version? [e.g. 580.126.09]: ' version
+    if [ -z $version ]
+      then echo "invalid version: {emtpy}; abort!"
+      exit
+    else
+      arch=$(uname -m)
+      url="https://us.download.nvidia.com/XFree86/Linux-$arch/$version/NVIDIA-Linux-$arch-$version.run"
+      echo "trying to fetch driver-version $version for $arch"
+      #echo "deleting old files..."
+      #rm NVIDIA-Linux-* 2>/dev/null
+    fi
+    if wget -q --spider "$url" > /dev/null; then
+      wget $url
+    else
+      echo "404: Download @ $url not found; abort!"
+      exit
+    fi
+    echo "Download complete, running installer"
+    localfile=$(ls | grep NVIDIA-Linux | tail -1)
   fi
-  if [ -z $version ]
-    then echo "invalid version: {emtpy}; abort!"
-    exit
-  else
-    arch=$(uname -m)
-    url="https://us.download.nvidia.com/XFree86/Linux-$arch/$version/NVIDIA-Linux-$arch-$version.run"
-    echo "trying to fetch driver-version $version for $arch"
-    #echo "deleting old files..."
-    #rm NVIDIA-Linux-* 2>/dev/null
-  fi
-  if wget -q --spider "$url" > /dev/null; then
-    wget $url
-  else
-    echo "404: Download @ $url not found; abort!"
-    exit
-  fi
-  echo "Download complete, running installer"
-  localfile=$(ls | grep NVIDIA-Linux | tail -1)
-fi
 chmod +x $localfile.run
 ./$localfile.run --module-signing-secret-key=/root/module-signing/MOK-nvidia.priv --module-signing-public-key=/root/module-signing/signing-nvidia.x509
 read -p 'finished, cleanup installer files? (input "keep" to keep downloaded files): ' keep
